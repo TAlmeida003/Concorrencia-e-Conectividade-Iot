1ced8cb7aacf0197b5941936ec9a953210e5be37
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2024-04-17 08:53:16 -0300

    ✨ feat(APP): Adição da API REST.

--- interfaces/Broker/APP.py
+++ interfaces/Broker/APP.py
@@ -1,42 +1,72 @@
 from flask import Flask, jsonify, request
-from Serve import Serve
+from Server import Server
 
-serve: Serve = Serve()
+serve: Server = Server()
 app = Flask(__name__)
 
 
-# GET Listar dispositivos
-# GET dispositivo
-# GET listar opções do dispositivo
-# GET opções ex.: temperatura
-
-# PUT opções do dispositivo ex.: ligar
-
-
-@app.route('/devices', methods=['GET'])
+@app.route('/devices', methods=['GET'])  # GET Listar dispositivos
 def get_devices():
+    if request.method != 'GET':
+        return jsonify({"mensagem": "Esta rota aceita apenas solicitações GET"}), 405
     return jsonify(serve.get_list_devices())
 
 
-@app.route('/devices/<string:ip>', methods=['GET'])
+@app.route('/devices/<string:ip>', methods=['GET'])  # GET dispositivo
 def get_device(ip: str):
+    if request.method != 'GET':
+        return jsonify({"mensagem": "Esta rota aceita apenas solicitações GET"}), 405
+
     try:
         return jsonify(serve.get_device(ip))
     except RuntimeError as e:
         return jsonify({"mensagem": e.__str__()}), 400
 
 
-@app.route('/devices/<string:ip>/<string:opcao>', methods=['GET'])
-def get_option_device(ip: str, opcao: str):
+@app.route('/devices/<string:ip>/opcoes', methods=['GET'])  # GET listar opções do dispositivo
+def get_options_device(ip: str):
+    if request.method != 'GET':
+        return jsonify({"mensagem": "Esta rota aceita apenas solicitações GET"}), 405
+
     try:
-        return jsonify(serve.get_device_option(ip, opcao))
+        return jsonify(serve.get_list_options(ip))
     except RuntimeError as e:
         return jsonify({"mensagem": e.__str__()}), 400
 
 
-# @app.route('/devices/<string:ip>/<string:opcao>', methods=['PUT'])
-# def put_option_device(ip: str, opcao: str):
-#     try:
-#         return jsonify(serve.get_device_option(ip, opcao))
-#     except RuntimeError as e:
-#         return jsonify({"mensagem": e.__str__()}), 400
+@app.route('/devices/<string:ip>/<string:opcao>', methods=['GET'])  # GET listar opções do dispositivo
+def get_option_device(ip: str, opcao: str):
+    try:
+        serve.check_method_option(ip, opcao, request.method)
+        dict_resp: dict = serve.get_device_option(ip, opcao)
+        if not dict_resp["success"]:
+            return jsonify(dict_resp), dict_resp["code"]
+        return jsonify(dict_resp)
+    except RuntimeError as e:
+        return jsonify({"mensagem": e.__str__()}), 400
+
+
+@app.route('/devices/<string:ip>/<string:opcao>', methods=['POST'])  # GET opções ex.: ligar
+def put_option_device(ip: str, opcao: str):
+    try:
+        serve.check_method_option(ip, opcao, request.method)
+        dict_resp: dict = serve.get_device_option(ip, opcao)
+        if not dict_resp["success"]:
+            return jsonify(dict_resp), dict_resp["code"]
+        return jsonify(dict_resp)
+    except RuntimeError as e:
+        return jsonify({"mensagem": e.__str__()}), 400
+
+
+# PUT opções do dispositivo ex.: ligar
+@app.route('/devices/<string:ip>/<string:opcao>/<string:value>', methods=['POST'])  # GET opções ex.: temperatura
+def put_option_device_value(ip: str, opcao: str, value: str):
+    try:
+        serve.check_method_option(ip, opcao, request.method)
+
+        dict_resp: dict = serve.get_device_option(ip, opcao, value)
+        if not dict_resp["success"]:
+            return jsonify(dict_resp), dict_resp["code"]
+        return jsonify(dict_resp)
+    except RuntimeError as e:
+        return jsonify({"mensagem": e.__str__()}), 400
