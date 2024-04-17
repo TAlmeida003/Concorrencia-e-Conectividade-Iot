ccdfd9a74477d1d3eda126808c8802898b1ef60b
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2024-04-17 18:58:08 -0300

    ✨ feat(Broker): Adição do recebimento de dados de maneira assíncrona.

---
+++ interfaces/Broker/Server/__init__.py
@@ -0,0 +1,128 @@
+import socket
+import threading
+
+HOST: str = socket.gethostbyname(socket.gethostname())
+PORT_TCP: int = 5001
+PORT_UDP: int = 5000
+
+
+class Server:
+    def __init__(self) -> None:
+        self.__dictConnectDevices__: dict = {}
+
+        self.__serve_udp__ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__serve_tcp__ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect_device(self) -> None:
+        try:
+            self.__serve_tcp__.bind((HOST, PORT_TCP))
+            self.__serve_udp__.bind((HOST, PORT_UDP))
+
+            self.__serve_tcp__.listen()
+        except socket.error:
+            raise RuntimeError("Servidor não foi inicializado.")
+
+        print("Servidor online")
+        print(f"Número de IP: {self.__serve_tcp__.getsockname()[0]} \n")
+        threading.Thread(target=self.thread_connect_device).start()
+
+    def thread_connect_device(self) -> None:
+        while True:
+            device_socket, addr = self.__serve_tcp__.accept()
+            ip: str = device_socket.getpeername()[0]
+
+            dict_device: dict = {"ip": ip, "socket": device_socket, "data_udp": {}, "opcoes": {}}
+            self.__dictConnectDevices__[ip] = dict_device
+            self.__dictConnectDevices__[ip]["opcoes"] = self.get_device_option(ip, "opcoes")["option"]
+
+            threading.Thread(target=self.get_recv_udp, args=[ip]).start()
+
+    def get_device_option(self, ip: str, option: str, value: str = "") -> dict:
+        pack_msg: dict = {"option": option, "value": value}
+        try:
+            return self.get_dict(pack_msg.__str__(), ip)
+        except KeyError:
+            raise RuntimeError("Nao existe nenhum dispositivo com esse ip.")
+
+    def get_dict(self, option: str, ip: str) -> dict:
+        try:
+            self.__dictConnectDevices__[ip]["socket"].send(option.encode())
+
+            self.__dictConnectDevices__[ip]["socket"].settimeout(30)
+            if self.is_command_utp(option, ip):
+                return eval(self.__dictConnectDevices__[ip]["socket"].recv(2048).decode('utf-8'))
+
+            self.__dictConnectDevices__[ip]["flag_data"] = False
+            while not self.__dictConnectDevices__[ip]["flag_data"]:
+                pass
+            return self.__dictConnectDevices__[ip]["data_udp"]
+        except socket.timeout:
+            raise RuntimeError("Tempo limite excedido")
+        except socket.error:
+            self.__dictConnectDevices__[ip]["socket"].close()
+            self.__dictConnectDevices__.pop(ip)
+            raise RuntimeError("Nao foi possível acessar o dispositivo.")
+
+    def get_recv_udp(self, ip: str) -> None:
+        while True:
+            try:
+                data, addr = self.__serve_udp__.recvfrom(2048)
+                data = data.decode()
+                self.__dictConnectDevices__[ip]["data_udp"] = eval(data)
+                self.__dictConnectDevices__[ip]["flag_data"] = True
+            except socket.error:
+                self.__dictConnectDevices__[ip]["socket"].close()
+                self.__dictConnectDevices__.pop(ip)
+                break
+
+    def get_device(self, ip: str) -> dict:
+        pack_msg: dict = {"option": "data", "value": ""}
+        try:
+            return self.get_dict(pack_msg.__str__(), ip)
+        except KeyError as e:
+            raise RuntimeError("Nao existe nenhum dispositivo com esse ip.")
+
+    def get_list_devices(self) -> list:
+        list_connect = []
+        list_ip: list = list(self.__dictConnectDevices__.keys())
+        for ip_device in list_ip:
+            if self.is_connect(ip_device):
+                list_connect.append(ip_device)
+        return list_connect
+
+    def is_connect(self, ip_device) -> bool:
+        try:
+            pack_msg: dict = {"option": "teste", "value": ""}
+            self.__dictConnectDevices__[ip_device]["socket"].send(pack_msg.__str__().encode())
+            return True
+        except socket.error:
+            self.__dictConnectDevices__[ip_device]["socket"].close()
+            self.__dictConnectDevices__.pop(ip_device)
+            return False
+
+    def end(self) -> None:
+        self.__serve_tcp__.close()
+        self.__serve_udp__.close()
+
+    def is_command_utp(self, option: str, ip) -> bool:
+        for i in self.__dictConnectDevices__[ip]["opcoes"]:
+            if eval(option)["option"] == i[0] and (not i[1]):
+                return False
+        return True
+
+    def get_list_options(self, ip: str) -> dict:
+        list_options: list = []
+        try:
+            for i in self.__dictConnectDevices__[ip]["opcoes"]:
+                list_options.append(i[0])
+            return {"options": list_options}
+        except KeyError as e:
+            raise RuntimeError("Nao existe nenhum dispositivo com esse ip.")
+
+    def check_method_option(self, ip: str, option: str, method):
+        try:
+            for i in self.__dictConnectDevices__[ip]["opcoes"]:
+                if option == i[0] and method != i[2]:
+                    raise RuntimeError("Esta rota aceita apenas solicitações " + i[2])
+        except KeyError as e:
+            raise RuntimeError("Nao existe nenhum dispositivo com esse ip.")
