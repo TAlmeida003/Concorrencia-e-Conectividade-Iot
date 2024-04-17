150009ad9d5ccc1a21e845e42571c46811d09185
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2024-04-17 18:59:12 -0300

    ✨ feat(Sensor): reorganização da lista de opções.

--- interfaces/Sensor/Sensor/__init__.py
+++ interfaces/Sensor/Sensor/__init__.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST: str = '192.168.0.104'
+HOST: str = '10.65.136.191'
 PORT_TCP: int = 5001
 PORT_UDP: int = 5000
 
@@ -15,9 +15,13 @@ class Sensor:
         # Atributos de conexão
         self.__connected__: bool = False
         self.__name__: str = "SETNA05"
+        self.__tag__: str = "sensor"
         self.__IP__: str = socket.gethostbyname(socket.gethostname())
-        self.__server_options__: list[str] = ["ligar", "desligar", "temperatura-atual", "umidade-atual",
-                                              "reiniciar"]
+        self.__server_options__: list[tuple[str, bool, str]] = [("ligar", True, "POST"),
+                                                                ("desligar", True, "POST"),
+                                                                ("temperatura-atual", False, "GET"),
+                                                                ("umidade-atual", False, "GET"),
+                                                                ("reiniciar", True, "POST")]
         self.__exe_serve_atual__: str = ""
 
         # Inicialização das conexões TCP e UDP
@@ -29,12 +33,10 @@ class Sensor:
         try:
             if not self.__connected__:
                 self.__tcp_connection__: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                #self.__udp_connection__: socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 self.__tcp_connection__.connect((HOST, PORT_TCP))
                 self.__connected__ = True
             else:
                 raise RuntimeError("O broker já está conectado.")
-
         except socket.error:
             self.disconnectBroker()
             raise RuntimeError("Não foi possível conectar ao Broker.")
@@ -69,20 +71,17 @@ class Sensor:
             raise RuntimeError("Broker desconectado")
 
     def turnOn(self) -> None:
-        # Ligar sensor
         if self.__state__:
             raise RuntimeError("O sensor já está ligado.")
         self.__state__ = True
 
     def turnOff(self) -> None:
-        # Desligar sensor
         if not self.__state__:
             raise RuntimeError("O sensor já está desligado.")
 
         self.__state__ = False
 
     def setTemperature(self, value: float) -> None:
-        # Definir temperatura
         if not self.__state__:
             raise RuntimeError("O sensor está desligado.")
         elif value > 60:
@@ -92,7 +91,6 @@ class Sensor:
         self.__temperature__ = value
 
     def setHumidity(self, value: float) -> None:
-        # Definir umidade
         if not self.__state__:
             raise RuntimeError("O sensor está desligado.")
         elif value > 80:
@@ -102,27 +100,22 @@ class Sensor:
         self.__humidity__ = value
 
     def setName(self, name: str) -> None:
-        # Definir nome do sensor
         if not name:
             raise RuntimeError("Nome invalido.")
         elif len(name) != 7:
             raise RuntimeError("Nome deve ter 7 caracteres")
-
         self.__name__ = name
 
     def restart(self) -> None:
         try:
             self.turnOff()
             self.turnOn()
-        except RuntimeError as e:
+        except RuntimeError:
             raise RuntimeError("Sensor não pode ser reiniciado.")
 
-    def get_exe_option(self) -> str:
-        return self.__exe_serve_atual__
-
     def is_continuous_mod(self) -> bool:
-        return ((self.__exe_serve_atual__ == self.__server_options__[2]) or
-                (self.__exe_serve_atual__ == self.__server_options__[3]))
+        return ((self.__exe_serve_atual__ == self.__server_options__[2][0]) or
+                (self.__exe_serve_atual__ == self.__server_options__[3][0]))
 
     def get_temperature(self) -> float:
         if not self.__state__:
@@ -135,21 +128,23 @@ class Sensor:
         return self.__humidity__
 
     def set_option_serve(self, option: str) -> None:
-        if (option not in self.__server_options__) and option != "data" and option != "teste":
+        if not self.is_option(option) and option != "data" and option != "teste" and option != "opcoes":
             raise RuntimeError("opção invalida.")
-        if not ((option == self.__server_options__[2]) or (option == self.__server_options__[3]) or option == "teste"):
-            self.__exe_serve_atual__ = option
-        elif self.__exe_serve_atual__ == self.__server_options__[2] and option == self.__server_options__[3]:
-            self.__exe_serve_atual__ = option
-        elif self.__exe_serve_atual__ == self.__server_options__[3] and option == self.__server_options__[2]:
-            self.__exe_serve_atual__ = option
+        self.__exe_serve_atual__ = option
 
     def get_list_options(self) -> list:
         return self.__server_options__
+
+    def is_option(self, option: str) -> bool:
+        for i in self.__server_options__:
+            if i[0] == option:
+                return True
+        return False
 
     def get_info(self) -> dict:
         # Obter informações do sensor
         return {"IP": self.__IP__,
                 "nome": self.__name__,
                 "opções": self.__server_options__,
+                "tag": self.__tag__
                 }
