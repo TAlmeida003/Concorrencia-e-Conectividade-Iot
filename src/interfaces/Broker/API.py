06fb8d405ca0de44effb0646b707193ed852f7a9
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2024-04-20 21:21:50 -0300

    🚚chore(API): renomeando o arquivo API.

--- src/interfaces/Broker/API.py
+++ src/interfaces/Broker/API.py
@@ -8,30 +8,30 @@ app = Flask(__name__)
 @app.route('/devices', methods=['GET'])  # GET Listar dispositivos
 def get_devices():
     if request.method != 'GET':
-        return jsonify({"mensagem": "Esta rota aceita apenas solicitações GET"}), 405
+        return jsonify({"success": False, "descript": "Esta rota aceita apenas solicitações GET"}), 405
     return jsonify(serve.get_list_devices())
 
 
 @app.route('/devices/<string:ip>', methods=['GET'])  # GET dispositivo
 def get_device(ip: str):
     if request.method != 'GET':
-        return jsonify({"mensagem": "Esta rota aceita apenas solicitações GET"}), 405
+        return jsonify({"success": False, "descript": "Esta rota aceita apenas solicitações GET"}), 405
 
     try:
         return jsonify(serve.get_device(ip))
     except RuntimeError as e:
-        return jsonify({"mensagem": e.__str__()}), 400
+        return jsonify({"success": False, "descript": e.__str__()}), 400
 
 
 @app.route('/devices/<string:ip>/opcoes', methods=['GET'])  # GET listar opções do dispositivo
 def get_options_device(ip: str):
     if request.method != 'GET':
-        return jsonify({"mensagem": "Esta rota aceita apenas solicitações GET"}), 405
+        return jsonify({"success": False, "descript": "Esta rota aceita apenas solicitações GET"}), 405
 
     try:
         return jsonify(serve.get_list_options(ip))
     except RuntimeError as e:
-        return jsonify({"mensagem": e.__str__()}), 400
+        return jsonify({"success": False, "descript": e.__str__()}), 400
 
 
 @app.route('/devices/<string:ip>/<string:opcao>', methods=['GET'])  # GET listar opções do dispositivo
@@ -43,7 +43,7 @@ def get_option_device(ip: str, opcao: str):
             return jsonify(dict_resp), dict_resp["code"]
         return jsonify(dict_resp)
     except RuntimeError as e:
-        return jsonify({"mensagem": e.__str__()}), 400
+        return jsonify({"success": False, "descript": e.__str__()}), 400
 
 
 @app.route('/devices/<string:ip>/<string:opcao>', methods=['POST'])  # GET opções ex.: ligar
@@ -55,7 +55,7 @@ def put_option_device(ip: str, opcao: str):
             return jsonify(dict_resp), dict_resp["code"]
         return jsonify(dict_resp)
     except RuntimeError as e:
-        return jsonify({"mensagem": e.__str__()}), 400
+        return jsonify({"success": False, "descript": e.__str__()}), 400
 
 
 # PUT opções do dispositivo ex.: ligar
@@ -69,4 +69,4 @@ def put_option_device_value(ip: str, opcao: str, value: str):
             return jsonify(dict_resp), dict_resp["code"]
         return jsonify(dict_resp)
     except RuntimeError as e:
-        return jsonify({"mensagem": e.__str__()}), 400
+        return jsonify({"success": False, "descript": e.__str__()}), 400
